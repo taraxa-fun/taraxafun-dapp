50b8a04d97bcbdc44c89d77e3304601f0e3b99a3
rms-devv <ramos.mcontact@gmail.com>
2024-12-12 11:15:49 +0100

    update fav icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,29 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Taraxa Fun</title>
+        <meta content="" name="Taraxafun" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+
         {/* Preconnect pour Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         {/* Lien pour Google Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Knewave&family=Manrope:wght@200..800&display=swap"
