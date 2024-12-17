e9a1249b6fa25902d2f9752be54b16244f80f2a2
rms-devv <ramos.mcontact@gmail.com>
2024-12-17 10:52:16 +0100

    update serv url

--- src/components/Auth/InitAuth.tsx
+++ src/components/Auth/InitAuth.tsx
@@ -7,26 +7,23 @@ export const InitAuth = () => {
   const { signMessageAsync } = useSignMessage();
   const { initAuth, jwt, userMe, fetchUserMe, logout, verifyJwt } = useAuthStore();
 
-  // Vérification du JWT au chargement
   useEffect(() => {
     if (jwt) {
       const isJwtValid = verifyJwt();
-      if (!isJwtValid) {
-        logout(); // Nettoie le state si le JWT n'est plus valide
+      if (!isJwtValid) { 
+        logout(); 
       } else if (!userMe) {
-        fetchUserMe(); // Récupère les données utilisateur si le JWT est valide
+        fetchUserMe(); 
       }
     }
   }, [jwt, verifyJwt, logout, userMe, fetchUserMe]);
 
-  // Déconnexion si userMe existe et l'utilisateur est déconnecté du wallet
   useEffect(() => {
     if (userMe && !address) {
       logout();
     }
   }, [address, userMe, logout]);
-
-  // Authentification seulement si nécessaire
+  
   useEffect(() => {
     if (!jwt && address && signMessageAsync) {
       initAuth(address, signMessageAsync);
